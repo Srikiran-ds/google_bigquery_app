3f5d0159499395e8fc3e43de5395129a31c1fe96
Srikiran Goud Chagapuram <64151178+Srikiran-ds@users.noreply.github.com>
2025-06-29 22:32:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,14 +36,43 @@ def insert_query(query,name,age):
 # Create a connection object.
 st.title("ABC Steel Data Input Form")
 tab1, tab2, tab3 = st.tabs(["Update", "Read", "Analysis"])
-df = pd.DataFrame(run_query("SELECT * FROM `top-athlete-459808-j9.name_age_dataset.name_age`"))
+#df = pd.DataFrame(run_query("SELECT * FROM `top-athlete-459808-j9.name_age_dataset.name_age`"))
+df = pd.DataFrame(run_query("SELECT * FROM `top-athlete-459808-j9.maintenance_date.log_data_sample`"))
 if tab2.button("refresh"):
-    df = pd.DataFrame(run_query("SELECT * FROM `top-athlete-459808-j9.name_age_dataset.name_age`"))
+    df = pd.DataFrame(run_query("SELECT * FROM `top-athlete-459808-j9.maintenance_date.log_data_sample`"))
 tab2.write(df)
 
 # Add the new vendor data to the existing data
-name = tab1.text_input(label="Name",value=None)
-age = tab1.number_input(label="age",min_value=0,value=None)
+#name = tab1.text_input(label="Name",value=None)
+#age = tab1.number_input(label="age",min_value=0,value=None)
+date = tab1.date_input("Entry date", value="today")
+time=tab1.time_input("Time", value="now")
+shift = tab1.selectbox(
+    "Shift",
+    ("SHIFT-A", "SHIFT-B", "SHIFT-C"),
+)
+incharge = tab1.text_input("SHIFT INCHARGE NAME", "")
+wash_flag = tab1.selectbox(
+    "WASHING & GREASING",
+    ("YES", "NO"),
+)
+vehicle = tab1.selectbox(
+    "VEHICLE NO",
+    ("TATA EX 19", "TATA EX 16","TATA EX 18","LG EX 21"),
+)
+vehicle_type = tab1.text_input("VEHICLE TYPE", "EXCAVATOR")
+break_cause_flag = tab1.selectbox(
+    "BRAKEDOWN CAUSE",
+    ( "NO","YES"),
+)
+break_maintenance_flag = tab1.selectbox(
+    "BREAKDOWN MAINTENANCE",
+    ( "NO","YES"),
+)
+preventive_maintenance_flag = tab1.selectbox(
+    "PREVENTIVE MAINTENANCE",
+    ("YES", "NO"),
+)
 submit_button = tab1.button(label="Submit Details")
 if submit_button:
     #insert = "
@@ -57,7 +86,8 @@ if submit_button:
     #query_job_kai_insert =insert_query(f"""INSERT INTO `light-willow-459806-t7.sample_name_data.name_age` (Name, Age) VALUES ('India', '75')""")
     #query_job_kai_insert =run_query("select * from `light-willow-459806-t7.sample_name_data.name_age` where age > 20")
     #query_job_kai_insert =insert_query("insert into `top-athlete-459808-j9.name_age_dataset.name_age` values (:name,:age)",name,age)
-    query_job_kai_insert =client.query(f"""insert into `top-athlete-459808-j9.name_age_dataset.name_age` values ('{name}',{age})""")
+    #query_job_kai_insert =client.query(f"""insert into `top-athlete-459808-j9.name_age_dataset.name_age` values ('{name}',{age})""")
+    query_job_kai_insert =client.query(f"""insert into `top-athlete-459808-j9.maintenance_date.log_data_sample` values ('{date}','{time}','{shift}','{incharge}','{wash_flag}','{vehicle}','{vehicle_type}','{break_cause_flag}','{break_maintenance_flag}','{preventive_maintenance_flag}')""")
     #tab1.write(query_job_kai_insert)
     tab1.success('Record added Successfully')
     # Update Google Sheets with the new vendor data
@@ -68,14 +98,14 @@ if submit_button:
     #df = conn.read(worksheet="sample", ttl="0.5m")
     #st.write(df)
 #tab3.write("Mean of ages")
-tab3.metric("Mean Age",df.Age.mean())
+#tab3.metric("Mean Age",df.Age.mean())
 tab3.metric("#Entries",df.shape[0])
-tab3.line_chart(df.Age)
+#tab3.line_chart(df.Age)
 # Create a histogram
-fig = px.histogram(df, x="Age")
+#fig = px.histogram(df, x="Age")
 
 # Display the figure
-tab3.plotly_chart(fig)
+#tab3.plotly_chart(fig)
 #rows2 = pd.DataFrame(run_query("SELECT * FROM `top-athlete-459808-j9.name_age_dataset.name_age`"))
 #st.write()
 #rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
